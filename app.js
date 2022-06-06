b9946ed63b049b4fa39f4930e2653b102f0ba506
Dillon Vuong <dillonv@uci.edu>
2022-06-05 22:29:28 -0700

    undici to fetch projects implemented

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-require('dotenv').config()
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static("public"))
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static("public"));
 
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 
-app.listen(process.env.PORT || 3000)
+app.listen(3000);
---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    "HACKADAY_API_URL": "https://api.hackaday.io/v1/"
+};
+
+module.exports = config;
